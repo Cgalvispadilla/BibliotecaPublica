13d85066a6470dda3ac61d5c989577d0c1675099
Cgalvispadilla <fktcg99@gmail.com>
2021-10-12 15:26:31 -0500

    se crearon todas las reglas de negocio en el servicio del modelo Resource

--- src/main/java/com/sofkau/biblioteca/repositories/ResourceRepository.java
+++ src/main/java/com/sofkau/biblioteca/repositories/ResourceRepository.java
@@ -4,6 +4,11 @@ import com.sofkau.biblioteca.models.Resource;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ResourceRepository extends MongoRepository<Resource, String> {
+    List<Resource> findAllByType(String type);
+    List<Resource> findAllBytheme(String theme);
+
 }
---
+++ src/main/java/com/sofkau/biblioteca/services/ResourceService.java
@@ -0,0 +1,111 @@
+package com.sofkau.biblioteca.services;
+
+import com.sofkau.biblioteca.dtos.ResourceDTO;
+import com.sofkau.biblioteca.mappers.ResourceMapper;
+import com.sofkau.biblioteca.models.Resource;
+import com.sofkau.biblioteca.repositories.ResourceRepository;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class ResourceService {
+    private ResourceRepository repository;
+    private ResourceMapper mapper;
+
+    public ResourceService(ResourceRepository repository, ResourceMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public List<ResourceDTO> getAll() {
+        List<ResourceDTO> resourceDTOS = new ArrayList<>();
+        repository.findAll().forEach(resource -> resourceDTOS.add(mapper.convertToDto(resource)));
+        return resourceDTOS;
+    }
+
+    public ResourceDTO save(ResourceDTO resourceDTO) {
+        if (resourceDTO.getName().isEmpty()) {
+            throw new IllegalArgumentException("el nombre no puede estar vacio");
+        }
+        Resource resource = mapper.convertToDocument(resourceDTO);
+        return mapper.convertToDto(repository.save(resource));
+    }
+
+    public ResourceDTO getById(String id) {
+        Optional<Resource> resource = repository.findById(id);
+        if (resource.isEmpty()) {
+            throw new NoSuchElementException("no existe un recurso con el id: " + id);
+        }
+        return mapper.convertToDto(resource.get());
+    }
+
+    public void delete(String id) {
+        repository.delete(mapper.convertToDocument(getById(id)));
+    }
+
+    public ResourceDTO update(ResourceDTO resourceDTO) {
+        Resource resource = mapper.convertToDocument(resourceDTO);
+        getById(resource.getId());
+        return mapper.convertToDto(resource);
+    }
+
+    public String checkAvailability(String id) {
+        ResourceDTO dto = getById(id);
+        if (isAvailable(dto)) {
+            return "El recurso " + dto.getName() + " esta disponible";
+        }
+        return "el recurso " + dto.getName() + " no esta disponible " + " fue prestado " + dto.getLoanDate();
+    }
+
+    private boolean isAvailable(ResourceDTO dto) {
+        return dto.getQuantityAvailable() > dto.getQuantityBorrowed();
+    }
+
+    public String lend(String id) {
+        ResourceDTO dto = getById(id);
+        if (isAvailable(dto)) {
+            dto.setQuantityBorrowed(dto.getQuantityBorrowed() + 1);
+            dto.setLoanDate(LocalDate.now());
+            update(dto);
+            return "El recurso " + dto.getName() + " se ha prestado";
+        }
+        return "el recurso " + dto.getName() + " no esta disponible para prestarse";
+    }
+
+    public List<ResourceDTO> recommendByType(String type) {
+        List<ResourceDTO> resourceDTOS = new ArrayList<>();
+        repository.findAllByType(type).forEach(resource -> resourceDTOS.add(mapper.convertToDto(resource)));
+        return resourceDTOS;
+    }
+
+    public List<ResourceDTO> recommendByTheme(String theme) {
+        List<ResourceDTO> resourceDTOS = new ArrayList<>();
+        repository.findAllBytheme(theme).forEach(resource -> resourceDTOS.add(mapper.convertToDto(resource)));
+        return resourceDTOS;
+    }
+
+    public List<ResourceDTO> recommendByTypeAndTheme(String type, String theme) {
+        List<ResourceDTO> resourceDTOS = new ArrayList<>();
+        List<ResourceDTO> resourceAux = new ArrayList<>();
+        resourceAux.addAll(recommendByTheme(theme));
+        resourceAux.addAll(recommendByType(type));
+        resourceAux.stream().distinct().forEach(resourceDTO -> resourceDTOS.add(resourceDTO));
+        return resourceDTOS;
+    }
+
+    public String returnResource(String id) {
+        ResourceDTO dto = getById(id);
+        if (dto.getQuantityBorrowed() > 0) {
+            dto.setQuantityBorrowed(dto.getQuantityBorrowed() - 1);
+            update(dto);
+            return "El recurso " + dto.getName() + " se ha devuelto";
+        }
+        return "el recurso " + dto.getName() + " no se puede devolver, porque no se ha prestado";
+    }
+}
